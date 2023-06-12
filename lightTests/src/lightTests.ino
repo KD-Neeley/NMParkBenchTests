370c6e2033ba2d9838e5d87c104be6dae99ec796
Katie Neeley <kdneeley@Katies-MacBook-Pro-4.local>
2023-06-11 18:09:38 -0600

    Error: Not seeing the LPD8806.h in library

---
+++ lightTests/src/lightTests.ino
@@ -0,0 +1,30 @@
+/*
+ * Project lightTests
+ * Description: Light tests for the NM Park Bench Project
+ * Author: KD Neeley
+ * Date: 06/11/2023
+ */
+#include <LPD8806.h>
+
+const int NLEDS = 160; // Number of LEDs
+const int CLKPIN = 14; //Clock In Pin
+const int DPIN = 15; //Data In Pin
+
+LPD8806 lights = LPD8806(NLEDS, CLKPIN, DPIN);
+
+void setup() {
+    lights.begin();
+    lights.show();
+}
+
+void loop() {
+    lights.setPixelColor(0, 255, 255, 255);
+    lights.show();
+}
+
+
+
+
+
+
+
