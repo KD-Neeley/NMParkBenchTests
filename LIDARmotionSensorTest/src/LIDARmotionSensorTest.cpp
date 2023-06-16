9cce7562ecbedbc6799ce01270e6fc13950ffba0
Katie Neeley <kdneeley@Katies-MacBook-Pro-4.local>
2023-06-16 11:05:09 -0600

    Trying to get the motionsensor test to work

--- LIDARmotionSensorTest/src/LIDARmotionSensorTest.cpp
+++ LIDARmotionSensorTest/src/LIDARmotionSensorTest.cpp
@@ -18,23 +18,23 @@ void loop();
 #line 10 "/Users/kdneeley/Documents/IoT/NMParkBenchTests/LIDARmotionSensorTest/src/LIDARmotionSensorTest.ino"
 const int I2CADDRESS = 0X62;
 
-byte * arrayToSave[2];
+byte * arrayToSave[16];
 LIDARLite L1;
 
 
 void setup() {
  // Initialize Arduino serial port (for display of ASCII output to PC)
     Serial.begin(9600);
+    Wire.begin();
     L1.begin(0, true);
 }
 
 void loop() {
     int dist;
-
-    dist = L1.distance(true, I2CADDRESS); 
-    L1.write(0x04, 0x00, I2CADDRESS);
+    L1.write(0x03, 0x00, I2CADDRESS);
     L1.read(0x01, 1, arrayToSave[0], true, I2CADDRESS);
+    dist = L1.distance(true, I2CADDRESS);
     // Display distance
-    Serial.print(dist);
+    Serial.printf("Distance Measured = %i\n", dist);
 
 }
